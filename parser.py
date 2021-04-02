d8d6c2bbbf9fc658a258156298d4c3ffeda9945e
Dreyoung_Lee <jlucslzy@163.com>
2021-04-02 08:34:01 +0800

    update the function of parsing expressions, statements and declarations

---
+++ parser.py
@@ -0,0 +1,67 @@
+
+"""
+parse expressions
+"""
+
+
+def match_mul():
+    return
+
+
+def match_add():
+    return
+
+
+def match_rel():
+    return
+
+
+def match_logic():
+    return
+
+
+"""
+parse statements
+"""
+def parse_simple_statement():
+    return
+
+
+def parse_assign_statement():
+    return
+
+
+def parse_return_statement():
+    return
+
+
+def parse_IO_statement():
+    return
+
+
+def parse_loop_statement():
+    return
+
+
+def parse_condition_statement():
+    return
+
+
+"""
+parse declarations
+"""
+
+
+def parse_variable():
+    return
+
+
+def parse_type():
+    return
+
+
+def parse_procedure():
+    return
+
+
+
